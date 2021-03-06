bd577996ac7ad0d9fce7a097d9712edcfe17c326
Phuc. Tran Nguyen Hoang <phuctnh@vng.com.vn>
2021-03-06 12:13:41 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: "Backend Site",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "264632695",
+      },
+    },
+  ],
+};
